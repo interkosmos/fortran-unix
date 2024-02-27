976927bbd7038f1239da51628c1383367cdf390a
Philipp <pengel@hs-nb.de>
2024-02-27 21:36:24 +0100

    Updated example.

--- examples/stat/stat.f90
+++ examples/stat/stat.f90
@@ -13,26 +13,48 @@ program main
 
     character(len=:), allocatable :: atime, mtime, ctime
 
-    integer           :: rc
-    type(c_ptr)       :: ptr
-    type(c_stat_type) :: file_stat
+    integer                 :: file_type, rc
+    integer(kind=c_int64_t) :: file_mode
+    type(c_stat_type)       :: file_stat
 
+    ! Get file status.
     rc = c_stat(FILE_NAME // c_null_char, file_stat)
+    if (rc /= 0) error stop
 
-    if (rc == 0) then
-        ptr = c_ctime(file_stat%st_atim%tv_sec)
-        call c_f_str_ptr(ptr, atime)
+    print '("File name........: ", a)',  FILE_NAME
+    print '("File size........: ", i0)', file_stat%st_size
 
-        ptr = c_ctime(file_stat%st_mtim%tv_sec)
-        call c_f_str_ptr(ptr, mtime)
+    ! Get file type.
+    file_mode = c_uint_to_int(file_stat%st_mode)
+    file_type = int(iand(file_mode, int(S_IFMT, kind=c_int64_t)))
 
-        ptr = c_ctime(file_stat%st_ctim%tv_sec)
-        call c_f_str_ptr(ptr, ctime)
+    write (*, '("File type........: ")', advance='no')
 
-        print '("File name........: ", a)',   FILE_NAME
-        print '("File size........: ", i0)',  file_stat%st_size
-        print '("File access time.: ", a24)', atime
-        print '("File modify time.: ", a24)', mtime
-        print '("File changed time: ", a24)', ctime
-    end if
+    select case (file_type)
+        case (S_IFBLK)
+            print '("block device")'
+        case (S_IFCHR)
+            print '("character device")'
+        case (S_IFDIR)
+            print '("directory")'
+        case (S_IFIFO)
+            print '("fifo")'
+        case (S_IFLNK)
+            print '("symlink")'
+        case (S_IFREG)
+            print '("file")'
+        case (S_IFSOCK)
+            print '("socket")'
+        case default
+            print '("unknown")'
+    end select
+
+    ! Get file times.
+    call c_f_str_ptr(c_ctime(file_stat%st_atim%tv_sec), atime)
+    call c_f_str_ptr(c_ctime(file_stat%st_mtim%tv_sec), mtime)
+    call c_f_str_ptr(c_ctime(file_stat%st_ctim%tv_sec), ctime)
+
+    print '("File access time.: ", a24)', atime
+    print '("File modify time.: ", a24)', mtime
+    print '("File changed time: ", a24)', ctime
 end program main
